05d5830ce76dca39b6717091623944f205c42960
whibi1 <104696288+whibi1@users.noreply.github.com>
2023-03-13 22:32:51 +0300

    Update atm_system.py

--- atm/atm_system.py
+++ atm/atm_system.py
@@ -14,18 +14,18 @@ You can exit the program with the 'q' key.
 balance  = 1000 # Let's say our balance is $1000.
 
 while True:
-    Transaction = input("Please enter transaction:")
+    transaction = input("Please enter transaction:")
 
-    if (Transaction == "q"):
+    if (transaction == "q"):
         print("We are waiting for you soon :)")
         break
-    elif (Transaction == "1"):
+    elif (transaction == "1"):
         print("Your Balance is {} $".format(balance))
-    elif (Transaction == "2"):
+    elif (transaction == "2"):
         amount = int(input("The amount you want to deposit:"))
 
         balance += amount
-    elif (Transaction == "3"):
+    elif (transaction == "3"):
         amount = int(input("Please enter the amount you want to withdraw:"))
         if (balance - amount < 0 ):
             print("You cannot withdraw that much money.")
